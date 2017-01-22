165d8208f857a9e2a970f7d762e67a923ad133a5
Matt Sadauckas <matthewsadauckas@gmail.com>
2017-01-21 21:27:44 -0700

    feat(core) Create init components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <JustButtons />
+      <ButtonsWithSlider />
+    </div>
+  )
 }
 
 export default App;
+
+const JustButtons = () => {
+  return (
+    <div>
+      <h2>Buttons will go here</h2>
+    </div>
+  )
+}
+
+const ButtonsWithSlider = () => {
+  return (
+    <div>
+      <h2>Buttons with slider will go here</h2>
+    </div>
+  )
+}
