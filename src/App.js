a36a905fd5d40188f6857ae431f7918ae44facfa
Matt Sadauckas <matthewsadauckas@gmail.com>
2017-01-24 16:31:30 -0700

    feat(core) Switch to dropdown sliders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { changeVal } from './actions'
-import { initVal } from './reducers'
+// import { initVal } from './reducers'
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import Slider from 'material-ui/Slider'
 
-// import DropDownMenu from 'material-ui/DropDownMenu'
-// import MenuItem from 'material-ui/MenuItem'
+import DropDownMenu from 'material-ui/DropDownMenu'
+import MenuItem from 'material-ui/MenuItem'
 
 const getCurrentState = state => state
 
@@ -25,7 +25,7 @@ let App = (state) => {
         {keys.map( (key, i) =>
         <MuiThemeProvider
           key={i + 'a'}>
-          <SliderInput
+          <DropDownSliderInput
           key={i + 'b'}
           id={key}
           name={metricValues[key].name}
@@ -48,38 +48,7 @@ App = connect(getCurrentState)(App)
 
 export default App;
 
-let SliderInput = ({ dispatch, id, name, value }) => {
-
-  const onChange = (e, input) => {
-    e.preventDefault()
-    let output = {
-      id,
-      val: input,
-      name,
-    }
-    dispatch(changeVal(output))
-  }
-
-  return (
-    <div className="input">
-      <div className="metricName">
-        <h2>{name}: {value.toFixed(2)}</h2>
-      </div>
-      <Slider
-        className="slider"
-        step={0.25}
-        min={1}
-        max={9}
-        defaultValue={initVal}
-        onChange={onChange}
-        />
-    </div>
-  )
-}
-
-SliderInput = connect()(SliderInput)
-
-// let DropDownSliderInput = ({ dispatch, id, name, value }) => {
+// let SliderInput = ({ dispatch, id, name, value }) => {
 //
 //   const onChange = (e, input) => {
 //     e.preventDefault()
@@ -91,46 +60,77 @@ SliderInput = connect()(SliderInput)
 //     dispatch(changeVal(output))
 //   }
 //
-//   const styles = {
-//     label: {
-//       fontSize: 20,
-//     },
-//     menu: {
-//       width: 250,
-//     },
-//     customWidth: {
-//       // marginLeft: 0,//effects dropped down menu
-//       fontSize: 20,
-//       paddingLeft: 0,
-//     }
-//   }
-//
 //   return (
 //     <div className="input">
-//       <DropDownMenu
-//         value={1}
-//         autoWidth={false}
+//       <div className="metricName">
+//         <h2>{name}: {value.toFixed(2)}</h2>
+//       </div>
+//       <Slider
 //         className="slider"
-//         labelStyle={styles.label}
-//         menuStyle={styles.menu}
-//         style={styles.customWidth}
-//         >
-//         <MenuItem
-//           value={1}
-//           primaryText={ name +': ' + value.toFixed(2)}
-//           className="dropDownMenu">
-//           <Slider
-//             step={0.25}
-//             min={1}
-//             max={9}
-//             defaultValue={value}
-//             onChange={onChange}
-//             />
-//
-//         </MenuItem>
-//       </DropDownMenu>
+//         step={0.25}
+//         min={1}
+//         max={9}
+//         defaultValue={initVal}
+//         onChange={onChange}
+//         />
 //     </div>
 //   )
 // }
 //
-// DropDownSliderInput = connect()(DropDownSliderInput)
+// SliderInput = connect()(SliderInput)
+
+let DropDownSliderInput = ({ dispatch, id, name, value }) => {
+
+  const onChange = (e, input) => {
+    e.preventDefault()
+    let output = {
+      id,
+      val: input,
+      name,
+    }
+    dispatch(changeVal(output))
+  }
+
+  const styles = {
+    label: {
+      fontSize: 20,
+    },
+    menu: {
+      width: 250,
+    },
+    customWidth: {
+      // marginLeft: 0,//effects dropped down menu
+      fontSize: 20,
+      paddingLeft: 0,
+    }
+  }
+
+  return (
+    <div className="input">
+      <DropDownMenu
+        value={1}
+        autoWidth={false}
+        className="slider"
+        labelStyle={styles.label}
+        menuStyle={styles.menu}
+        style={styles.customWidth}
+        >
+        <MenuItem
+          value={1}
+          primaryText={ name +': ' + value.toFixed(2)}
+          className="dropDownMenu">
+          <Slider
+            step={0.25}
+            min={1}
+            max={9}
+            defaultValue={value}
+            onChange={onChange}
+            />
+
+        </MenuItem>
+      </DropDownMenu>
+    </div>
+  )
+}
+
+DropDownSliderInput = connect()(DropDownSliderInput)
