a87d6dc01052b151ca7309b74890925a5ed14de1
Matt Sadauckas <matthewsadauckas@gmail.com>
2017-01-24 16:04:25 -0700

    feat(core) Remove elements causing warnings for slider elements

--- src/App.js
+++ src/App.js
@@ -7,17 +7,15 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import Slider from 'material-ui/Slider'
 
-import DropDownMenu from 'material-ui/DropDownMenu'
-import MenuItem from 'material-ui/MenuItem'
+// import DropDownMenu from 'material-ui/DropDownMenu'
+// import MenuItem from 'material-ui/MenuItem'
 
 const getCurrentState = state => state
 
 let App = (state) => {
 
   const metricValues = getCurrentState(state).metricValues
-  // console.log('metric values:', metricValues);
   const keys = Object.keys(metricValues).sort()
-  // console.log(keys.sort());
 
   return (
     <div className="reportInput">
@@ -27,7 +25,7 @@ let App = (state) => {
         {keys.map( (key, i) =>
         <MuiThemeProvider
           key={i + 'a'}>
-          <DropDownSliderInput
+          <SliderInput
           key={i + 'b'}
           id={key}
           name={metricValues[key].name}
@@ -54,7 +52,6 @@ let SliderInput = ({ dispatch, id, name, value }) => {
 
   const onChange = (e, input) => {
     e.preventDefault()
-    // console.log('on change value:', input);
     let output = {
       id,
       val: input,
@@ -82,58 +79,58 @@ let SliderInput = ({ dispatch, id, name, value }) => {
 
 SliderInput = connect()(SliderInput)
 
-let DropDownSliderInput = ({ dispatch, id, name, value }) => {
-
-  const onChange = (e, input) => {
-    e.preventDefault()
-    let output = {
-      id,
-      val: input,
-      name,
-    }
-    dispatch(changeVal(output))
-  }
-
-  const styles = {
-    label: {
-      fontSize: 20,
-    },
-    menu: {
-      width: 250,
-    },
-    customWidth: {
-      // marginLeft: 0,//effects dropped down menu
-      fontSize: 20,
-      paddingLeft: 0,
-    }
-  }
-
-  return (
-    <div className="input">
-      <DropDownMenu
-        value={1}
-        autoWidth={false}
-        className="slider"
-        labelStyle={styles.label}
-        menuStyle={styles.menu}
-        style={styles.customWidth}
-        >
-        <MenuItem
-          value={1}
-          primaryText={ name +': ' + value.toFixed(2)}
-          className="dropDownMenu">
-          <Slider
-            step={0.25}
-            min={1}
-            max={9}
-            defaultValue={initVal}
-            onChange={onChange}
-            />
-
-        </MenuItem>
-      </DropDownMenu>
-    </div>
-  )
-}
-
-DropDownSliderInput = connect()(DropDownSliderInput)
+// let DropDownSliderInput = ({ dispatch, id, name, value }) => {
+//
+//   const onChange = (e, input) => {
+//     e.preventDefault()
+//     let output = {
+//       id,
+//       val: input,
+//       name,
+//     }
+//     dispatch(changeVal(output))
+//   }
+//
+//   const styles = {
+//     label: {
+//       fontSize: 20,
+//     },
+//     menu: {
+//       width: 250,
+//     },
+//     customWidth: {
+//       // marginLeft: 0,//effects dropped down menu
+//       fontSize: 20,
+//       paddingLeft: 0,
+//     }
+//   }
+//
+//   return (
+//     <div className="input">
+//       <DropDownMenu
+//         value={1}
+//         autoWidth={false}
+//         className="slider"
+//         labelStyle={styles.label}
+//         menuStyle={styles.menu}
+//         style={styles.customWidth}
+//         >
+//         <MenuItem
+//           value={1}
+//           primaryText={ name +': ' + value.toFixed(2)}
+//           className="dropDownMenu">
+//           <Slider
+//             step={0.25}
+//             min={1}
+//             max={9}
+//             defaultValue={value}
+//             onChange={onChange}
+//             />
+//
+//         </MenuItem>
+//       </DropDownMenu>
+//     </div>
+//   )
+// }
+//
+// DropDownSliderInput = connect()(DropDownSliderInput)
