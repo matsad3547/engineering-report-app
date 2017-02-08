8a04145dbebf855ea08a21dabfd25f25a1d8ff95
Matt Sadauckas <matthewsadauckas@gmail.com>
2017-02-07 17:05:15 -0700

    chore(refactor) Rearrange component files

---
+++ src/components/ReportsDisplay/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton'
+import ReportItem from '../../components/ReportItem'
+
+const ReportsDisplay = ({ reports,
+                          status,
+                          queued,
+                          queueReport,
+                          unqueueReport,
+                          downloadQueued }) => {
+
+  const firstReport = 0
+  const lastReport = 10
+
+  const download = e => {
+    e.preventDefault()
+    downloadQueued(reports, queued)
+
+  }
+
+  const styles = {
+    button: {
+      height: 50,
+      width: 250,
+      margin: 10,
+    },
+  }
+
+  if (status === 'received') {
+    let keys = Object.keys(reports)
+    keys = keys.sort( (a, b) => b - a )
+    const selectedKeys = keys.slice(firstReport, lastReport)
+
+    return (
+      <div className="existingReports">
+        <h3>Most Recent Reports</h3>
+        <div className="reportList">
+          {selectedKeys.map( (k, i) =>
+            <ReportItem
+              key={i}
+              report={k}
+              index={i}
+              queued={queued}
+              queueReport={queueReport}
+              unqueueReport={unqueueReport}
+              />)}
+        </div>
+
+        <RaisedButton
+          label="download"
+          style={styles.button}
+          onClick={download}
+          />
+      </div>
+    )
+  }
+  else if (status === 'errored') {
+    return (
+      <div className="existingReports">
+        <h3>Report loading has failed, sorry!</h3>
+      </div>
+    )
+  }
+  return (
+    <div className="existingReports">
+      <h3>Reports are loading...</h3>
+    </div>
+  )
+}
+
+export default ReportsDisplay
--- src/containers/ExistingReports/index.js
+++ src/containers/ExistingReports/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import { connect } from 'react-redux'
-import RaisedButton from 'material-ui/RaisedButton'
-import ReportItem from '../../components/ReportItem'
 import { queueReport, unqueueReport } from '../../actions/'
+import ReportsDisplay from '../../components/ReportsDisplay'
 
 export const formatReport = report => {
   let parsedReport = []
@@ -28,17 +26,16 @@ export const formatReport = report => {
 }
 
 export const parseCSV = data => {
+  console.log('parsing csv');
   let csvContent = 'data:text/csv;charset=utf-8,'
   data.forEach( (arr, i) => {
     const dataString = arr.join(',')
     csvContent += i < data.length ? dataString + '\n' : dataString
   })
-  console.log('csv:', csvContent);
   return csvContent
 }
 
-const launchDownload = data => {
-  const csvContent = parseCSV(data)
+const launchDownload = csvContent => {
   const encodedUri = encodeURI(csvContent)
   const link = document.createElement('a')
   link.setAttribute('href', encodedUri)
@@ -48,78 +45,11 @@ const launchDownload = data => {
 }
 
 export const downloadQueued = (reports, queued) => {
-  const dataArr = queued.map( q => {
-    console.log('report:', reports[q].config);
-    return formatReport(reports[q])
+  const dataStr = queued.map( q => {
+    let data = formatReport(reports[q])
+    return parseCSV(data)
   })
-  launchDownload(dataArr)
-}
-
-const ReportsDisplay = ({ reports,
-                          status,
-                          queued,
-                          queueReport,
-                          unqueueReport,
-                          downloadQueued }) => {
-
-  const firstReport = 0
-  const lastReport = 10
-
-  const download = e => {
-    e.preventDefault()
-    const data = [['test1','test2', 'stuff'], ['things', 'stuff', 'poop']]
-    downloadQueued(reports, queued)
-
-  }
-
-  const styles = {
-    button: {
-      height: 50,
-      width: 250,
-      margin: 10,
-    },
-  }
-
-  if (status === 'received') {
-    let keys = Object.keys(reports)
-    keys = keys.sort( (a, b) => b - a )
-    const selectedKeys = keys.slice(firstReport, lastReport)
-
-    return (
-      <div className="existingReports">
-        <h3>Most Recent Reports</h3>
-        <div className="reportList">
-          {selectedKeys.map( (k, i) =>
-            <ReportItem
-              key={i}
-              report={k}
-              index={i}
-              queued={queued}
-              queueReport={queueReport}
-              unqueueReport={unqueueReport}
-              />)}
-        </div>
-
-        <RaisedButton
-          label="download"
-          style={styles.button}
-          onClick={download}
-          />
-      </div>
-    )
-  }
-  else if (status === 'errored') {
-    return (
-      <div className="existingReports">
-        <h3>Report loading has failed, sorry!</h3>
-      </div>
-    )
-  }
-  return (
-    <div className="existingReports">
-      <h3>Reports are loading...</h3>
-    </div>
-  )
+  launchDownload(dataStr)
 }
 
 const mapStateToProps = state => {
