9df2ae611b0a9fa315aee96b019520a345ca536c
Matt Sadauckas <matthewsadauckas@gmail.com>
2017-03-31 20:53:50 -0600

    feat(core) Added about page with version number

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => (
+  <div className="color flexLayout welcome">
+    <h1>Engineering Report App</h1>
+    <p>Built to help test engineers in the field.</p>
+    <p>version 0.01.01</p>
+  </div>
+)
+
+export default About
